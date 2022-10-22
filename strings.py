4066ad800bef48177bc1a16adc783bb0af39e42e
VladimirR07 <Vova.ru0007@yandex.ru>
2022-10-22 13:33:50 +0500

    bot telegramm1

---
+++ strings.py
@@ -0,0 +1,17 @@
+BOT_TOKEN_FILENAME = 'token.txt'
+
+
+SYMBOL_X = '❎'
+SYMBOL_O = '⭐'
+SYMBOL_UNDEF = '◻'
+
+
+ANSW_YOUR_TURN = 'Ваш ход'
+ANSW_YOU_WIN = 'Вы победили'
+ANSW_BOT_WIN = 'Победил бот'
+ANSW_DRAW = 'Ничья'
+ANSW_HELP = 'Бот для игры в крестики-нолики\n\n' \
+            'Используйте команду /start для начала новой игры'
+
+
+ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимать можно только на ' + SYMBOL_UNDEF
